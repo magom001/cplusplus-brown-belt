c7f1062648fc99fc1d3c8edb4400c1b1b8ccc4bf
magom001 <magom001@yandex-team.ru>
2018-12-07 11:00:19 +0300

    Programming Assignment: Вторичный индекс в базе данных 3.0

--- DatabaseIndex/Database.h
+++ DatabaseIndex/Database.h
@@ -1,17 +1,9 @@
-#pragma once
+#include <iostream>
+#include <map>
 #include <string>
 #include <unordered_map>
-#include <unordered_set>
-#include <map>
-#include <list>
-
-#include <iostream>
 
 using namespace std;
-using User = string;
-using ID = string;
-using Karma = int;
-using Timestamp = int;
 
 struct Record {
     string id;
@@ -24,102 +16,91 @@ struct Record {
 class Database {
 public:
     bool Put(const Record& record) {
-        if (id_index.find(record.id) == id_index.end()) {
-            const auto new_rec_it = storage_.insert(storage_.end(), record);
-            id_index[record.id] = new_rec_it;
+        auto [it, inserted] = storage.insert(
+                {record.id, Data {record, {}, {}, {}}}
+        );
 
-            UpdateIndices(record, new_rec_it);
-            return true;
-        }
-
-        return false;
-    };
-
-    const Record* GetById(const string& id) const {
-        try {
-            return &(*id_index.at(id));
-        } catch(exception& ex) {
-            return nullptr;
-        }
-    };
-
-    bool Erase(const string& id) {
-        const auto r_it = id_index.find(id);
-        if (r_it == id_index.end()) {
+        if (!inserted) {
             return false;
         }
 
-        RemoveIndices(*(*r_it).second,(*r_it).second);
-        storage_.erase((*r_it).second);
-        id_index.erase(id);
-
+        auto& data = it->second;
+        const Record* ptr = &data.record;
+        data.timestamp_iter = timestamp_index.insert({record.timestamp, ptr});
+        data.karma_iter = karma_index.insert({record.karma, ptr});
+        data.user_iter = user_index.insert({record.user, ptr});
         return true;
-    };
+    }
+
+    const Record* GetById(const string& id) const {
+        auto it = storage.find(id);
+        if (it == storage.end()) {
+            return nullptr;
+        }
+
+        return &it->second.record;
+    }
+
+    bool Erase(const string& id) {
+        auto it = storage.find(id);
+        if (it == storage.end()) {
+            return false;
+        }
+
+        const auto& data = it->second;
+        timestamp_index.erase(data.timestamp_iter);
+        karma_index.erase(data.karma_iter);
+        user_index.erase(data.user_iter);
+        storage.erase(it);
+        return true;
+    }
 
     template <typename Callback>
-    void RangeByTimestamp(int low, int high, const Callback& callback) const {
-        auto lower_bound = timestamp_index.lower_bound(low);
-        auto upper_bound = timestamp_index.upper_bound(high);
-        for (auto it = lower_bound; it != upper_bound; ++it) {
-            const auto& rec_ptrs = (*it).second;
-            for ( auto rec_ptr : rec_ptrs) {
-                bool fetchNext = callback(*rec_ptr);
-
-                if (!fetchNext) {
-                    return;
-                }
+    void RangeByTimestamp(int low, int high, Callback callback) const {
+        auto it_begin = timestamp_index.lower_bound(low);
+        auto it_end = timestamp_index.upper_bound(high);
+        for (auto it = it_begin; it != it_end; ++it) {
+            if (!callback(*it->second)) {
+                break;
             }
         }
-    };
+    }
 
     template <typename Callback>
-    void RangeByKarma(int low, int high, const Callback& callback) const {
-        auto lower_bound = karma_index.lower_bound(low);
-        auto upper_bound = karma_index.upper_bound(high);
-        for (auto it = lower_bound; it != upper_bound; ++it) {
-            const auto& rec_ptrs = (*it).second;
-            for ( auto rec_ptr : rec_ptrs) {
-                bool fetchNext = callback(*rec_ptr);
-
-                if (!fetchNext) {
-                    return;
-                }
+    void RangeByKarma(int low, int high, Callback callback) const {
+        auto it_begin = karma_index.lower_bound(low);
+        auto it_end = karma_index.upper_bound(high);
+        for (auto it = it_begin; it != it_end; ++it) {
+            if (!callback(*it->second)) {
+                break;
             }
         }
-    };
+    }
 
     template <typename Callback>
-    void AllByUser(const string& user, const Callback& callback) const {
-        try {
-            for (auto rec_ptr: user_index.at(user)) {
-                bool fetchNext = callback(*rec_ptr);
-
-                if (!fetchNext) {
-                    return;
-                }
+    void AllByUser(const string& user, Callback callback) const {
+        auto [it_begin, it_end] = user_index.equal_range(user);
+        for (auto it = it_begin; it != it_end; ++it) {
+            if (!callback(*it->second)) {
+                break;
             }
-        } catch(exception& ex) {
-            return;
         }
+    }
+
+private:
+    template <typename Type>
+    using Index = multimap<Type, const Record*>;
+
+    struct Data {
+        Record record;
+        Index<int>::iterator timestamp_iter;
+        Index<int>::iterator karma_iter;
+        Index<string>::iterator user_iter;
     };
 
 private:
-    void UpdateIndices(const Record& r, list<Record>::iterator it) {
-         karma_index[r.karma].insert(&(*it));
-         timestamp_index[r.timestamp].insert(&(*it));
-        user_index[r.user].insert(&(*it));
-    }
-
-    void RemoveIndices(const Record& r, list<Record>::iterator it) {
-         karma_index[r.karma].erase(&(*it));
-         timestamp_index[r.timestamp].erase(&(*it));
-        user_index[r.user].erase(&(*it));
-    }
-
-    list<Record> storage_ = {};
-    unordered_map<ID, Record> db_ = {};
-    unordered_map<ID, list<Record>::iterator> id_index = {};
-    unordered_map<User, unordered_set<Record*>> user_index = {};
-    map<Karma, unordered_set<Record*>> karma_index = {};
-    map<Timestamp, unordered_set<Record*>> timestamp_index{};
+    unordered_map<string, Data> storage;
+    Index<int> timestamp_index;
+    Index<int> karma_index;
+    Index<string> user_index;
 };
